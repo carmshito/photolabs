26e7a54a1f8bf004ec3275d0a5cc496d8c74d231
Carmelia Shito <carmshito@hotmail.com>
2023-08-24 17:37:18 -0400

    refactor PhotoList component to render multiple PhotoListItems

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,25 +3,76 @@ import React from "react";
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
-const PhotoList = (props) => {
-  let photoList = [];
-  for (const photo of props.photos) {
-    photoList.push((
-      <li key={photo.id}>
-        <PhotoListItem 
-          city={photo.location.city}
-          country={photo.location.country}
-          imageSource={photo.urls.full}
-          username={photo.user.name}
-          profile={photo.user.profile}
-        />
-      </li>
-    ));
-  }
+const sampleDataForPhotoList = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    },
+    user: {
+      id: "1",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+  {
+    id: "2",
+    location: {
+      city: "Toronto",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    },
+    user: {
+      id: "2",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+  {
+    id: "3",
+    location: {
+      city: "Ottawa",
+      country: "Canada",
+    },
+    urls: {
+      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
+      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    },
+    user: {
+      id: "3",
+      username: "exampleuser",
+      name: "Joe Example",
+      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+    },
+  },
+];
+
+const PhotoList = () => {
   return (
     <ul className="photo-list">
-      
-      {photoList}
+      {sampleDataForPhotoList.map((photo) => {
+        return (
+          <li key={photo.id}>
+            <PhotoListItem
+              city={photo.location.city}
+              country={photo.location.country}
+              imageSource={photo.urls.full}
+              username={photo.user.name}
+              profile={photo.user.profile}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 };
