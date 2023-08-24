9940163d4c97f370b0eebb245057126ce881229f
Carmelia Shito <carmshito@hotmail.com>
2023-08-24 18:46:45 -0400

    add photo topics

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -23,7 +24,15 @@ const sampleDataForTopicList = [
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {sampleDataForTopicList.map((topic) => {
+        return (
+          <span key={topic.id}>
+            <TopicListItem
+              title={topic.title}
+            />
+          </span>
+        );
+      })}
     </div>
   );
 };
