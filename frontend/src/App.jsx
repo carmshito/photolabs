a9aab080fb1cead701639bc57420540eca0365c0
Carmelia Shito <carmshito@hotmail.com>
2023-08-24 21:53:51 -0400

    refactor to use mock data for photos and topics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import './App.scss';
-
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  console.log('photos', photos, 'topics', topics)
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute 
+        topics={topics}
+        photos={photos}
+      />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -6,11 +6,14 @@ import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
+  const { topics, photos } = props
   return (
     <div className="home-route">
-      <TopNavigation />
-      <PhotoList />
+      <TopNavigation 
+        topics={topics}/>
+      <PhotoList 
+        photos={photos}/>
     </div>
   );
 };
