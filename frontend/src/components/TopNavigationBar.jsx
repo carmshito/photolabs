d7397b418f483c472ac0c44d899feadcd976a617
Carmelia Shito <carmshito@hotmail.com>
2023-08-26 18:02:06 -0400

    move topics to right side navbar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // styling
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
 
 // components
 import TopicList from './TopicList';
@@ -11,17 +11,19 @@ import FavBadge from './FavBadge';
 const TopNavigation = (props) => {
 
   const { topics, favPhotos } = props;
-  
+
   // check favPhotos array to see if photos exist in array
   const isFavPhotoExist = favPhotos.length;
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics} />
-      <FavBadge isFavPhotoExist={isFavPhotoExist}/>
+      <div className="top-nav-bar__topics">
+        <TopicList topics={topics} />
+      </div>
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
